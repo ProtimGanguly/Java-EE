34afb4a1f08bfd9d67b4792a6da9ce3fad7ffd6a
Protim Ganguly <40965025+ProtimGanguly@users.noreply.github.com>
2019-01-20 00:42:06 +0000

    Add files via upload
    A simple Java EE web application project in Red Hat JBoss Developer Studio and package it and deploy it to EAP application server.

---
+++ Package and Deploy a Java EE Application/model/ExampleService.java
@@ -0,0 +1,20 @@
+package com.redhat.training.model;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ExampleService
+ */
+@Stateless
+@LocalBean
+public class ExampleService {
+
+    /**
+     * Default constructor. 
+     */
+    public ExampleService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
